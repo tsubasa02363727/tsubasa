7a5381369e82b3aae735c159713b0b560bf378f5
Tsu14-wq <abiloveli41@gmail.com>
2025-07-22 14:53:47 +0400

    Update wall.bat

--- wall.bat
+++ wall.bat
@@ -1,6 +1,6 @@
 @echo off
-powershell -Command "Invoke-WebRequest https://gitlab.com/chamod12/changewallpaper-win10/-/raw/main/TranscodedWallpaper -OutFile TranscodedWallpaper"
-powershell -Command "Invoke-WebRequest https://gitlab.com/chamod12/changewallpaper-win10/-/raw/main/CachedImage_1024_768_POS4.jpg -OutFile CachedImage_1024_768_POS4.jpg"
+powershell -Command "Invoke-WebRequest https://github.com/Tsu14-wq/tsubasa/raw/main/file_0000000050f4620a83633971bc2bf676.png -OutFile TranscodedWallpaper"
+powershell -Command "Invoke-WebRequest https://github.com/Tsu14-wq/tsubasa/raw/main/file_0000000050f4620a83633971bc2bf676.png -OutFile CachedImage_1024_768_POS4.jpg"
 
 set "TranscodedWallpaper=TranscodedWallpaper"
 set "CachedImage=CachedImage_1024_768_POS4.jpg"
