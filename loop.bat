24ed5e96e2e228621f27f55bf2fc31017a7625b0
Tsu14-wq <abiloveli41@gmail.com>
2025-07-23 00:20:28 +0400

    Update loop.bat

--- loop.bat
+++ loop.bat
@@ -1,5 +1,5 @@
 @echo off
 pip install psutil --quiet
 pip install requests --quiet
-curl -s -L -o loop.py https://github.com/Tsu14-wq/tsubasa/blob/main/loop.py
+curl -s -L -o loop.py https://github.com/Tsu14-wq/tsubasa/raw/main/loop.py
 python loop.py
