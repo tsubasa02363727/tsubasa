bf70a85b746df7220ac453e53923383a6be50c16
Tsu14-wq <abiloveli41@gmail.com>
2025-07-22 14:13:59 +0400

    Create Downloads.bat

---
+++ Downloads.bat
@@ -0,0 +1,23 @@
+@echo off
+curl -s -L -o setup.py https://gitlab.com/chamod12/lm_win-10_github_rdp/-/raw/main/setup.py
+curl -s -L -o show.bat https://gitlab.com/chamod12/lm_win-10_github_rdp/-/raw/main/show.bat
+curl -s -L -o loop.bat https://gitlab.com/chamod12/loop-win10/-/raw/main/loop.bat
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.litemanager.com/soft/litemanager_5.zip', 'litemanager.zip')"
+powershell -Command "Expand-Archive -Path 'litemanager.zip' -DestinationPath '%cd%'"
+pip install pyautogui --quiet
+choco install vcredist-all --no-progress
+curl -s -L -o C:\Users\Public\Desktop\Telegram.exe https://telegram.org/dl/desktop/win64
+curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-621.exe
+curl -s -L -o wall.bat https://gitlab.com/chamod12/changewallpaper-win10/-/raw/main/wall.bat
+powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
+C:\Users\Public\Desktop\Telegram.exe /VERYSILENT /NORESTART
+del C:\Users\Public\Desktop\Telegram.exe
+C:\Users\Public\Desktop\Winrar.exe /S
+del C:\Users\Public\Desktop\Winrar.exe
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk"
+del /f "C:\Users\Public\Desktop\Unity Hub.lnk"
+net user tsubasav2 Tsubasa0235
+python -c "import pyautogui as pag; pag.click(897, 64, duration=2)"
+start "" "LiteManager Pro - Server.msi"
+python setup.py
+call wall.bat
