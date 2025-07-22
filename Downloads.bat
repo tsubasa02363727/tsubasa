49467730526bc28ced33004e8ebff1facbbe14aa
Tsu14-wq <abiloveli41@gmail.com>
2025-07-22 14:54:50 +0400

    Update Downloads.bat

--- Downloads.bat
+++ Downloads.bat
@@ -8,7 +8,7 @@ pip install pyautogui --quiet
 choco install vcredist-all --no-progress
 curl -s -L -o C:\Users\Public\Desktop\Telegram.exe https://telegram.org/dl/desktop/win64
 curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-621.exe
-curl -s -L -o wall.bat https://gitlab.com/chamod12/changewallpaper-win10/-/raw/main/wall.bat
+curl -s -L -o wall.bat https://github.com/Tsu14-wq/tsubasa/blob/main/wall.bat
 powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
 C:\Users\Public\Desktop\Telegram.exe /VERYSILENT /NORESTART
 del C:\Users\Public\Desktop\Telegram.exe
