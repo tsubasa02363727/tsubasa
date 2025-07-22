c75cea8c88c428d7a7ea53f0ffd788f762255743
Tsu14-wq <abiloveli41@gmail.com>
2025-07-23 00:10:43 +0400

    Update Downloads.bat

--- Downloads.bat
+++ Downloads.bat
@@ -1,14 +1,14 @@
 @echo off
-curl -s -L -o setup.py https://github.com/Tsu14-wq/tsubasa/blob/main/setup.py
-curl -s -L -o show.bat https://github.com/Tsu14-wq/tsubasa/blob/main/show.bat
-curl -s -L -o loop.bat https://github.com/Tsu14-wq/tsubasa/blob/main/loop.bat
+curl -s -L -o setup.py https://github.com/Tsu14-wq/tsubasa/raw/main/setup.py
+curl -s -L -o show.bat https://github.com/Tsu14-wq/tsubasa/raw/main/show.bat
+curl -s -L -o loop.bat https://github.com/Tsu14-wq/tsubasa/raw/main/loop.bat
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.litemanager.com/soft/litemanager_5.zip', 'litemanager.zip')"
 powershell -Command "Expand-Archive -Path 'litemanager.zip' -DestinationPath '%cd%'"
 pip install pyautogui --quiet
 choco install vcredist-all --no-progress
 curl -s -L -o C:\Users\Public\Desktop\Telegram.exe https://telegram.org/dl/desktop/win64
 curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-621.exe
-curl -s -L -o wall.bat https://github.com/Tsu14-wq/tsubasa/blob/main/wall.bat
+curl -s -L -o wall.bat https://github.com/Tsu14-wq/tsubasa/raw/main/wall.bat
 powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
 C:\Users\Public\Desktop\Telegram.exe /VERYSILENT /NORESTART
 del C:\Users\Public\Desktop\Telegram.exe
